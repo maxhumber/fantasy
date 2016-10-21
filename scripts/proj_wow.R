f2f77f2767bc99ce5f11934adbccb7a05c67ae58
Max Humber <max.humber@gmail.com>
2016-10-21 08:49:01 -0400

    Clean up wow

--- scripts/proj_wow.R
+++ scripts/proj_wow.R
@@ -22,14 +22,5 @@ proj_wow <- function(players) {
         labs(title = "Players ROS", x = "Week", y = "Fantasy Points")
 }
 
-trade_target <- ros_mean %>% 
-    filter(name %in% c("Ben Roethlisberger", "Andy Dalton", "Alex Smith", "Eli Manning")) %>% 
-    mutate(team = c(2, 1, 1, 2)) 
-
-ros_target <- ros %>% 
-    filter(name %in% c("Ben Roethlisberger", "Andy Dalton", "Alex Smith", "Eli Manning"))
-
 # test
-proj_wow(c("Alex Smith", "Ben Roethlisberger"))
-proj_wow(c("Alex Smith", "Andy Dalton", "Tyrod Taylor"))
-proj_wow(c("Alex Smith", "Ben Roethlisberger", "Andy Dalton", "Eli Manning"))
+proj_wow(c("Ben Roethlisberger", "Tyrod Taylor", "Andy Dalton"))
