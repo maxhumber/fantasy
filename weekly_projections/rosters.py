022e3d4d15cd296387559494e4e5188f4f077af6
Max Humber <max.humber@gmail.com>
2018-09-10 12:44:25 -0400

    Janky AF

--- weekly_projections/rosters.py
+++ weekly_projections/rosters.py
@@ -4,9 +4,68 @@ from bs4 import BeautifulSoup
 import sqlite3
 import re
 from week import week
+from fuzzywuzzy import process, fuzz
+
+
+ALPHABET = [chr(i) for i in range(ord('A'),ord('Z')+1)]
+URL = 'https://www.footballdb.com/players/current.html'
+
+headers = {
+    'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)'
+        'AppleWebKit/537.36 (KHTML, like Gecko)'
+        'Chrome/50.0.2661.102 Safari/537.36'
+    }
+
+payloads = [{'letter': letter} for letter in ALPHABET]
+https://www.footballdb.com/players/current.html?pos=WR
+
+payload = {'pos': 'WR'}
+
+payload = payloads[0]
+
+response = requests.get(URL, params=payload, headers=headers)
+soup = BeautifulSoup(response.text, 'lxml')
+for s in soup.find_all(class_='td hidden-xs'):
+    s.extract()
+soup = soup.find_all('div', class_='td')
+
+players = []
+for s in soup:
+    try:
+        players.append(s.find('a')['title'])
+    except TypeError:
+        pass
+    except KeyError:
+        pass
+pd.DataFrame({'name': players})
+
+
+
+
+
 
 con = sqlite3.connect('projections.db')
 cur = con.cursor()
+
+position = 'QB'
+
+players = (
+    pd.read_sql(f'''
+        select
+        name,
+        position
+        from projections
+        where
+        source = 'Fantasy Sharks' and
+        week = {week}
+    ''', con)
+)
+
+def fuzzy_player(player, position):
+    choices = list(players[players['position'] == position]['name'].values)
+    return process.extract(player, choices=choices, scorer=fuzz.partial_ratio)[0][0]
+
 
 league = 4319624
 URL = f'http://fantasy.nfl.com/league/{league}/depthcharts'
@@ -32,8 +91,25 @@ df = (df['players'].apply(pd.Series)
     .to_frame('players')
 )
 df = df.reset_index().sort_values('Team', ascending=False)
-df.columns = ['team', 'position', 'player']
+df.columns = ['team', 'position', 'name']
+
+
+player = 'C. Meredith'
+position = 'WR'
+choices = list(players[players['position'] == position]['name'].values)
+process.extract(player, choices=choices, scorer=fuzz.partial_ratio)[0][0]
+
+help(process.extract)
+
+df['name2'] = df.apply(lambda row: fuzzy_player(row['name'], row['position']), axis=1)
+df
+
+
+
+df
+
+
 df['week'] = week
 df['fetched_at'] = pd.Timestamp('now')
 
-df.to_sql('rosters', con, if_exists='append', index=False)
+df.to_sql('rosters', con, if_exists='replace', index=False)
