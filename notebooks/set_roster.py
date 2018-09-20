0e8b3c06605fc8c473812ef77661f457a228c2e4
Max Humber <max.humber@gmail.com>
2018-09-20 09:45:11 -0400

    with sql injection

--- notebooks/set_roster.py
+++ notebooks/set_roster.py
@@ -6,50 +6,15 @@ import random
 
 from fantasy.utils.week import week
 
-con = sqlite3.connect('data/fantasy.db')
-cur = con.cursor()
-
-team = 'phantasy'
-week = 3
-season = 2018
-
-df = pd.read_sql(f'''
-    select
-    name,
-    position,
-    week,
-    season,
-    points,
-    range
-    from (
-    	select
-    	name,
-    	position,
-    	week,
-    	season,
-    	strftime('%Y-%m-%d', fetched_at) as fetched_at,
-    	round(avg(points), 1) as points,
-    	(round(min(points), 1) || '-' || round(max(points), 1)) as range,
-    	count(*) as count
-    	from projections
-    	where
-    	season = {season} and
-    	week = {week} and
-    	strftime('%Y-%m-%d', fetched_at) = (select max(strftime('%Y-%m-%d', fetched_at)) from projections)
-    	group by 1, 2, 3, 4, 5
-    	having count(*) > 1
-    ) projections
-    left join (
-        select
-        *
-        from rosters
-        where
-        strftime('%Y-%m-%d', fetched_at) =
-            (select max(strftime('%Y-%m-%d', fetched_at)) from rosters)
-    ) as rosters using (name, position)
-    where team = '{team}'
-    order by points desc
-''', con)
+POSITIONS = {
+    'QB': 1,
+    'RB': 2,
+    'WR': 3,
+    'TE': 1,
+    'FLEX': 3,
+    'K': 1,
+    'DEF': 1
+}
 
 class Player:
 
@@ -61,35 +26,80 @@ class Player:
     def __repr__(self):
         return f"Player('{self.name}', '{self.position}', {self.points})"
 
-players = [Player(row['name'], row['position'], row['points']) for index, row in df.iterrows()]
-random.shuffle(players)
+# df = pd.read_sql('select * from rosters limit 100', con)
+# df.to_sql('test', con, if_exists='replace', index=False)
+#
+# injection = "'phantasy'; DROP TABLE TEST; --"
+# team = injection
+# pd.read_sql(f'select * from rosters where team = {team}', con)
 
-remaining_players = sorted(players, key=lambda player: player.points, reverse=True)
+def read_projection_data(team, week, season):
+    sql = f'''
+        select
+        name,
+        position,
+        week,
+        season,
+        points,
+        range
+        from (
+            select
+            name,
+            position,
+            week,
+            season,
+            strftime('%Y-%m-%d', fetched_at) as fetched_at,
+            round(avg(points), 1) as points,
+            (round(min(points), 1) || '-' || round(max(points), 1)) as range,
+            count(*) as count
+            from projections
+            where
+            season = {season} and
+            week = {week} and
+            strftime('%Y-%m-%d', fetched_at) = (select max(strftime('%Y-%m-%d', fetched_at)) from projections)
+            group by 1, 2, 3, 4, 5
+            having count(*) > 1
+        ) projections
+        left join (
+            select
+            *
+            from rosters
+            where
+            strftime('%Y-%m-%d', fetched_at) =
+                (select max(strftime('%Y-%m-%d', fetched_at)) from rosters)
+        ) as rosters using (name, position)
+        where team = '{team}'
+        order by points desc
+    '''
+    return pd.read_sql(sql, con)
 
-positions = {
-    'QB': 1,
-    'RB': 2,
-    'WR': 3,
-    'TE': 1,
-    'FLEX': 3,
-    'K': 1,
-    'DEF': 1
-}
-positions = [item for sublist in [[k]*v for k,v in positions.items()] for item in sublist]
+def optimize_roster(team, week, season=2018, positions=POSITIONS):
+    df = read_projection_data(team, week, season)
 
-starters = []
-for position in positions:
-    for i, player in enumerate(remaining_players):
-        if player.position == position:
-            starters.append(remaining_players.pop(i))
-            break
+    players = [Player(row['name'], row['position'], row['points']) for index, row in df.iterrows()]
+    remaining_players = sorted(players, key=lambda player: player.points, reverse=True)
+    positions = [item for sublist in [[k]*v for k,v in positions.items()] for item in sublist]
 
-flex = len([p for p in positions if p == 'FLEX'])
-for i in range(flex):
-    for j, player in enumerate(remaining_players):
-        if player.position in ['WR', 'RB', 'TE']:
-            starters.append(remaining_players.pop(j))
-            break
+    starters = []
+    for position in positions:
+        for i, player in enumerate(remaining_players):
+            if player.position == position:
+                starters.append(remaining_players.pop(i))
+                break
 
-players
-starters
+    flex = len([p for p in positions if p == 'FLEX'])
+    for i in range(flex):
+        for j, player in enumerate(remaining_players):
+            if player.position in ['WR', 'RB', 'TE']:
+                starters.append(remaining_players.pop(j))
+                break
+
+    return starters
+
+if __name__ == '__main__':
+    con = sqlite3.connect('data/fantasy.db')
+    cur = con.cursor()
+    starters = optimize_roster('phantasy', week)
+    print('Starters:\n', starters)
+    print('\n')
+    print('Points: ', sum([player.points for player in starters]))
