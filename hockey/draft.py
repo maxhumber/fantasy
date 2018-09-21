f9d2054b7d97f67d96c15c84d3f997ccbd3a92eb
Max Humber <max.humber@gmail.com>
2018-09-21 16:20:55 -0400

    simplify score

--- hockey/draft.py
+++ hockey/draft.py
@@ -10,6 +10,11 @@ pd.options.display.max_rows = 50
 
 con = sqlite3.connect('hockey/hockey.db')
 
+def scale(x, out_range=[0.80, 1]):
+    domain = np.min(x), np.max(x)
+    y = (x - (domain[1] + domain[0]) / 2) / (domain[1] - domain[0])
+    return y * (out_range[1] - out_range[0]) + (out_range[1] + out_range[0]) / 2
+
 def load_data(season='2018-19'):
     df = pd.read_sql(f'''
         select
@@ -49,94 +54,44 @@ def load_data(season='2018-19'):
     df = df.groupby(['name', 'position'])[CATEGORIES].mean().reset_index()
     return df
 
-def calculate_vor(df, pool_size=10, starters={'C': 2, 'LW': 2, 'RW': 2, 'D': 4, 'G': 2}):
-    df = df.copy()
-    for position, slots in starters.items():
-        for category in CATEGORIES:
-            try:
-                replacement = (
-                    df[df['position'] == position]
-                    .sort_values(category, ascending=False)
-                    .head(slots * pool_size)
-                    [category]
-                    .mean()
-                )
-                df.loc[df['position'] == position, category] = df[category] - replacement
-            except ValueError:
-                pass
-            except KeyError:
-                pass
-    return df
-
-def scale_categories(df):
-    df = df.copy()
-    df[CATEGORIES] = (
-        df
-        .groupby('position')
-        [CATEGORIES]
-        .apply(lambda x: (x - x.min()) / (x.max() - x.min()))
-    ).fillna(0)
-    return df
-
-def scale(x, out_range=[0.80, 1]):
-    domain = np.min(x), np.max(x)
-    y = (x - (domain[1] + domain[0]) / 2) / (domain[1] - domain[0])
-    return y * (out_range[1] - out_range[0]) + (out_range[1] + out_range[0]) / 2
-
-def mod_categories(df, out_range=[0.80, 1]):
-    df = df.copy()
-    bias = pd.read_sql('select * from bias where feature not like "position%"', con)
-    bias['mod'] = bias[['coef']].apply(lambda x: scale(x, out_range))
-    bias = bias[['feature', 'mod']]
-    bias = bias.set_index('feature').iloc[:,0]
-    df[list(bias.keys())] *= bias
-    return df
-
-def calculate_score(df, starters, pool_size):
-    score_name = 'cat_score'
-    df = df.copy()
-    df[score_name] = df[CATEGORIES].sum(axis=1)
-
-    players = sum(starters.values())
-    skaters = sum([value for key, value in starters.items() if key != 'G'])
-    goalies = players - skaters
-
-    df[score_name] = df['cat_score'] / players
-    df[score_name] = np.where(df['position'] == 'G', df[score_name] / 2, df[score_name] / 10)
-
-    for position, slots in starters.items():
-        replacement = (
-            df[df['position'] == position]
-            .sort_values(score_name, ascending=False)
-            .head(slots * pool_size)
-            [score_name]
-            .mean()
-        )
-        df.loc[df['position'] == position, score_name] = df[score_name] - replacement
-
-    df['custom_rank'] = df[score_name].rank(method='average', ascending=False)
-
-    return df[['name', 'custom_rank', score_name]].sort_values(score_name, ascending=False)
-
 df = load_data()
+
 pool_size = 10
 starters = {'C': 2, 'LW': 2, 'RW': 2, 'D': 4, 'G': 2}
-vor = calculate_vor(df, pool_size, starters)
-cat = scale_categories(vor)
-mod = mod_categories(cat)
-scores = calculate_score(mod, starters, pool_size)
 
-half = pd.merge(vor, scores, how='left', on='name')
+df[CATEGORIES] = (
+    df
+    [CATEGORIES]
+    .apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+).fillna(0)
+
+bias = pd.read_sql('select * from bias where feature not like "position%"', con)
+bias['mod'] = bias[['coef']].apply(lambda x: scale(x, (0.8, 1)))
+bias = bias[['feature', 'mod']].set_index('feature').iloc[:,0]
+df[list(bias.keys())] *= bias
+
+df['score'] = df[CATEGORIES].sum(axis=1)
+
+players = sum(starters.values())
+skaters = sum([value for key, value in starters.items() if key != 'G'])
+goalies = players - skaters
+
+df['score'] = df['score'] / players
+df['score'] = np.where(df['position'] == 'G', df['score'] / 2, df['score'] / 10)
+
+for position, slots in starters.items():
+    replacement = (
+        df[df['position'] == position]
+        .sort_values('score', ascending=False)
+        .head(slots * pool_size)
+        ['score']
+        .mean()
+    )
+    df.loc[df['position'] == position, 'score'] = df['score'] - replacement
+
+df['custom_rank'] = df['score'].rank(method='average', ascending=False)
+df = df.sort_values('custom_rank')
 ranks = pd.read_sql('select name, rank from ranks', con)
-full = pd.merge(half, ranks, how='left', on='name')
-full['rank_arbitrage'] = full['rank'] - full['custom_rank']
-full = full.sort_values('custom_rank')
-
-name = 'Viktor Arvidsson'
-df[df['name'] == name]
-vor[vor['name'] == name]
-cat[cat['name'] == name]
-mod[mod['name'] == name]
-
-df = full
+df = pd.merge(df, ranks, how='left', on='name')
+df['rank_arbitrage'] = df['custom_rank'] - df['rank']
 df.to_csv('hockey/list.csv', index=False)
