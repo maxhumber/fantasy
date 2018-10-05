79e241d6d63d001a5ad0f5b326dbbc6d4897b986
Max Humber <max.humber@gmail.com>
2018-10-05 16:39:51 -0400

    remove manual oauth process

--- hockey/using_yfs.py
+++ hockey/using_yfs.py
@@ -21,86 +21,17 @@ api.get('league/386.l.74371/settings')
 api.get('game/386/players')
 api.get('game/nhl')
 
-# manual
-
-import json
-import os
-import requests
-import webbrowser
-
-authorize_url = 'https://api.login.yahoo.com/oauth2/request_auth'
-access_token_url = 'https://api.login.yahoo.com/oauth2/get_token'
-
-redirect_uri = 'oob'
-league_id = 74371
-base_url = 'https://fantasysports.yahooapis.com/fantasy/v2'
-
-### get initial tokens
-
-params = {
-    'client_id': client_id,
-    'client_secret': client_secret,
-    'redirect_uri': redirect_uri,
-    'response_type': 'code',
-    'language': 'en-us',
-}
-
-headers = {'Content-Type': 'application/json'}
-response = requests.post(authorize_url, params=params, headers=headers)
-
-webbrowser.open(response.url)
-code = input('Enter code: ')
-
-data = {
-    'client_id': client_id,
-    'client_secret': client_secret,
-    'redirect_uri': redirect_uri,
-    'code': code,
-    'grant_type': 'authorization_code',
-}
-
-response = requests.post(access_token_url, data=data)
-
-### set tokens
-
-data = response.json()
-access_token = data['access_token']
-refresh_token = data['refresh_token']
-
-### refresh tokens
-
-data = {
-    'client_id': client_id,
-    'client_secret': client_secret,
-    'redirect_uri': redirect_uri,
-    'refresh_token': refresh_token,
-    'grant_type': 'refresh_token',
-}
-
-response = requests.post(access_token_url, data=data)
-
-data = response.json()
-access_token = data['access_token']
-refresh_token = data['refresh_token']
-
 ### get roster
 
+import pandas as pd
 from collections import ChainMap
 
 league_id = 74371
 team_id = 3
-url = base_url + '/' + f'team/nhl.l.{league_id}.t.{team_id}/roster/players'
+end_point = f'team/nhl.l.{league_id}.t.{team_id}/roster/players'
+data = api.get(end_point)
 
-response = requests.get(url, params={'format':'json'}, headers={'Authorization': f'Bearer {access_token}'})
-data = response.json()
-
-players = data['fantasy_content']['team'][1]['roster']['0']['players']
-players['0']
-
-player = players['0']['player'][0]
-player = dict(ChainMap(*player))
-
-player = players['3']
+# clean
 
 def clean_player(player):
     player = player['player'][0]
@@ -114,11 +45,9 @@ def clean_player(player):
     }
     return player
 
+players = data['fantasy_content']['team'][1]['roster']['0']['players']
 keys = [k for k in players.keys() if k != 'count']
-
-import pandas as pd
-pd.DataFrame([clean_player(players[k]) for k in keys])
-
-clean_player(players['3'])
+players = [clean_player(players[k]) for k in keys]
+pd.DataFrame(players)
 
 #
