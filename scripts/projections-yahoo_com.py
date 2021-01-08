5cbaed8ce5d575bd6e84c3e1c059759dc00b3e51
Max Humber <max.humber@gmail.com>
2021-01-08 14:16:50 -0500

    $250

--- scripts/projections-yahoo_com.py
+++ scripts/projections-yahoo_com.py
@@ -6,21 +6,27 @@ import pandas as pd
 
 def parse_skater(tr):
     name = tr.find("a", {"href": "players"}, mode="list")[-1].text
-    stats = tr.find("td", {"class": "Ta-end"})[3:]
-    stats = [float(s.text) for s in stats]
-    labels = ["goals", "assists", "plus_minus", "powerplay_points", "shots_on_goal", "hits"]
-    data = dict(zip(labels, stats))
-    data["name"] = name
-    return data
+    data = [td.text for td in tr.find("td")][5:-1]
+    data = [float(d.replace("%", "")) for d in data]
+    labels = ["gp", "ranking_preseason", "ranking_current", "rostered", "goals", "assists", "plus_minus", "powerplay_points", "shots_on_goal", "hits"]
+    team, position = tr.find("span", {"class": "Fz-xxs"}, mode="first").text.split(" - ")
+    player = dict(zip(labels, data))
+    player["name"] = name
+    player["team"] = team
+    player["position"] = position
+    return player
 
 def parse_goalie(tr):
     name = tr.find("a", {"href": "players"}, mode="list")[-1].text
-    stats = tr.find("td", {"class": "Ta-end"})[3:]
-    stats = [float(s.text) for s in stats]
-    labels = ["wins", "goals_against", "goals_against_average", "saves", "shots_against", "save_percentage", "shutouts"]
-    data = dict(zip(labels, stats))
-    data["name"] = name
-    return data
+    data = [td.text for td in tr.find("td")][5:-1]
+    data = [float(d.replace("%", "")) for d in data]
+    labels = ["gp", "ranking_preseason", "ranking_current", "rostered", "wins", "goals_against", "goals_against_average", "saves", "shots_against", "save_percentage", "shutouts"]
+    team, position = tr.find("span", {"class": "Fz-xxs"}, mode="first").text.split(" - ")
+    player = dict(zip(labels, data))
+    player["name"] = name
+    player["team"] = team
+    player["position"] = position
+    return player
 
 def scrape(parse, position, offset=0):
     url = "https://hockey.fantasysports.yahoo.com/hockey/84570/players"
@@ -30,7 +36,7 @@ def scrape(parse, position, offset=0):
         cut_type=33,
         stat1="S_PSR",
         myteam=0,
-        sort="OR",
+        sort="AR",
         sdir=1,
         count=offset
     )
@@ -50,8 +56,6 @@ df = pd.concat([sdf, gdf])
 df["source"] = "Yahoo"
 df["fetched_at"] = pd.Timestamp("now")
 df["blocks"] = None
-df["team"] = None
-df["position"] = None
 
 df = df[
     [
@@ -60,6 +64,8 @@ df = df[
         "team",
         "position",
         "name",
+        "ranking_preseason",
+        "ranking_current",
         "goals",
         "assists",
         "plus_minus",
