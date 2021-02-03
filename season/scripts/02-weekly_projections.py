9583f40efa0af545fab15b4e9e0c9c4a6d4cfb03
bracketltd <github@bracket.ltd>
2021-02-02 22:11:37 -0500

    better bench warming

--- season/scripts/02-weekly_projections.py
+++ season/scripts/02-weekly_projections.py
@@ -3,8 +3,12 @@ import random
 from itertools import chain
 from gazpacho import Soup
 import pandas as pd
+import numpy as np
 
-from utils import week, yahoo
+pd.options.display.max_columns = 100
+pd.options.display.max_rows = 100
+
+from utils import yahoo
 
 
 schedule = pd.read_csv("season/data/nhl_team_schedule.csv")
@@ -21,7 +25,7 @@ def parse(tr):
     return {**player, **data}
 
 
-def scrape(league, team, count=0, position="P"):
+def scrape_page(league, team, count=0, position="P"):
     url = f"https://hockey.fantasysports.yahoo.com/hockey/{league}/players"
     params = dict(
         sort="AR",
@@ -33,30 +37,46 @@ def scrape(league, team, count=0, position="P"):
     )
     soup = Soup.get(url, params)
     trs = soup.find("div", {"id": "players-table"}).find("tr")[2:]
-    skaters = pd.DataFrame([parse(tr) for tr in trs])
-    skaters["league"] = league
-    skaters["owner"] = team
-    return skaters
+    df = pd.DataFrame([parse(tr) for tr in trs])
+    df["league"] = league
+    df["owner"] = team
+    return df
 
 
-def get_projections(week, yahoo):
+def scrape_league(league, team):
     df = pd.DataFrame()
-    for league, team in yahoo.items():
-        for team, count in [(team, 0), ("A", 0), ("A", 25)]:
-            skaters = scrape(league, team, count)
-            df = df.append(skaters)
-            time.sleep(random.uniform(1, 10) / 10)
-    df = pd.merge(df, schedule, how="left", on="team")
-    multiplier_current = (week / 13) # 13 total weeks
-    multiplier_draft = 1 - multiplier_current
-    df["ranking_draft"] *= multiplier_draft
-    df["ranking_current"] *= multiplier_current
-    df["rank"] = round((df["ranking_draft"] + df["ranking_current"]) / 2, 1)
-    df = df.sort_values("rank")
+    for team, count in [(team, 0), ("A", 0), ("A", 25), ("A", 50)]:
+        dp = scrape_page(league, team, count)
+        df = df.append(dp)
+        time.sleep(random.uniform(1, 10) / 10)
+    df = df.drop(["gp", "ranking_current", "rostered"], axis=1).reset_index()
+    return df
+
+
+def custom_rank(df):
+    dr = df[['name', 'goals', 'assists', 'powerplay_points', 'shots_on_goal', 'hits']]
+    dr = dr.set_index("name")
+    dr = dr.apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+    dr["rollup"] = dr.apply(lambda row: row.sum(), axis=1)
+    dr["ranking_custom"] = dr["rollup"].rank(ascending=False)
+    dr["target"] = ((dr["powerplay_points"] >= 0.20) & (dr["hits"] >= 0.20))
+    dr = dr.reset_index()[["name", "ranking_custom", "target"]]
+    df = pd.merge(df, dr, on="name", how="left")
+    df = df[[
+        'league', 'owner', 'name', 'team', 'position',
+        'ranking_draft', 'ranking_custom', 'target',
+        'goals', 'assists', 'plus_minus', 'powerplay_points', 'shots_on_goal', 'hits'
+    ]]
+    df = df.sort_values("ranking_custom")
     return df
 
 
 if __name__ == "__main__":
-    df = get_projections(week, yahoo)
+    df = pd.DataFrame()
+    for league, team in yahoo.items():
+        dl = scrape_league(league, team)
+        dr = custom_rank(dl)
+        df = df.append(dr)
+    df = pd.merge(df, schedule, how="left", on="team")
     df['fetched'] = pd.Timestamp("now")
     df.to_csv("season/data/weekly_projections.csv", index=False)
--- season/scripts/03-bench_warmers.py
+++ season/scripts/03-bench_warmers.py
@@ -5,7 +5,12 @@ pd.options.display.max_rows = 100
 from utils import flatten, week, yahoo
 
 
-def fill_roster(df, date):
+# load
+projections = pd.read_csv("season/data/weekly_projections.csv")
+
+
+def fill_roster(input_df, date):
+    df = input_df.copy().sort_values("ranking_custom")
     slots_to_fill = Counter({'C': 2, 'LW': 2, 'RW': 2, 'D': 4})
     roster = {k: [] for k in slots_to_fill.keys()}
     for i, row in df.iterrows():
@@ -18,11 +23,17 @@ def fill_roster(df, date):
                 if slots_to_fill.get(position) > 0:
                     slots_to_fill[position] -= 1
                     roster.get(position).append(name)
-    return roster, slots_to_fill
+    return roster
+
+# # testing
+# league, team = 84778, 12
+# df = projections[projections["league"] == league]
+# team = df[df["owner"] != "A"]
+# fill_roster(team, "2021-02-04")
 
 
-def simulate(df):
-    df = df.copy()
+def simulate(input_df):
+    df = input_df.copy()
     df["starts"] = 0
     dates = [
         date.strftime("%Y-%m-%d")
@@ -30,37 +41,66 @@ def simulate(df):
         if date >= pd.Timestamp("2020-01-01")
     ]
     for date in dates:
-        roster, slots = fill_roster(df, date)
+        roster = fill_roster(df, date)
         df.loc[df['name'].isin(flatten(roster)), "starts"] += 1
-    roster = df[["name", "position", "rank", "starts"]]
-    roster = roster.sort_values(["starts", "rank"], ascending=[False, True])
-    return roster
+    df["keeper"] = (
+        df["ranking_draft"].rank() +
+        df["ranking_custom"].rank() +
+        df["starts"].rank(ascending=False)
+    )
+    df["keeper"] = df[["keeper"]].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+    df = df.sort_values("keeper")
+    df["keeper"] = 1 - df["keeper"]
+    df = df[["name", "position", "ranking_draft", "ranking_custom", "starts", "keeper"]]
+    return df
+
+# # testing
+# league, team = 84778, 12
+# df = projections[projections["league"] == league]
+# team = df[df["owner"] != "A"]
+# simulate(team)
 
 
-def drop_and_add(df, starts, drop):
-    available = df[df["owner"] == "A"]
+def drop_and_add(projections_df, drop_player, current_starts):
+    df = projections_df.copy().sort_values("ranking_custom")
+    df["starts_marginal"] = 0
     team = df[df["owner"] != "A"]
-    team_after_drop = team[~team["name"].isin(drop)]
-    maybe = []
+    team_after_drop = team[~team["name"].isin(drop_player)]
+    available = df[df["owner"] == "A"].copy()
     for i, row in available.iterrows():
         possible_add = available.iloc[i:i+1]
         team_after_possible = pd.concat([team_after_drop, possible_add])
         roster_possible = simulate(team_after_possible)
-        starts_possible = roster_possible['starts'].sum()
-        starts_marginal = starts_possible - starts
-        if starts_marginal > 0:
-            maybe.append((row["name"], row["position"], row["rank"], starts_marginal))
-    return maybe
+        starts_marginal = roster_possible['starts'].sum() - current_starts
+        available.loc[available['name'] == row["name"], "starts_marginal"] = starts_marginal
+    df = available
+    df = df[
+        (df["starts_marginal"] >= 0) &
+        (df["target"] == True)
+    ]
+    df = df[["name", "position", "ranking_custom", "starts_marginal"]]
+    df = df.sort_values(["starts_marginal", "ranking_custom"], ascending=[False, True])
+    return df
 
+# # testing
+# league, team = 84778, 12
+# df = projections[projections["league"] == league]
+# starts = 41
+# drop = ["Nick Ritchie"]
+# drop_and_add(df, drop, starts)
 
-projections = pd.read_csv("season/data/weekly_projections.csv")
 
 for league, team in yahoo.items():
     df = projections[projections["league"] == league]
     team = df[df["owner"] != "A"]
     roster = simulate(team)
     starts = roster['starts'].sum()
-    drop = roster.tail(1)[["name", "position", "rank"]]
-    adds = drop_and_add(df, starts, drop)
-    print(f"League: {league},\nDrop: {drop},\nAdd:\n", adds)
+    drop = roster.tail(1)[["name"]]
+    wire = drop_and_add(df, drop, starts)
+    print(f"League: {league}\n")
+    print(f"Roster:")
+    print(roster)
+    print("\n")
+    print(f"Wire:")
+    print(wire.head(10))
     print("\n")
