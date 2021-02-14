9d7692454cb31f338fb6b45ed1df5fd1f41da51a
Max Humber <max.humber@gmail.com>
2021-02-13 22:13:52 -0500

    goalies

--- season/tweak.py
+++ season/tweak.py
@@ -3,24 +3,38 @@ import pandas as pd
 from season.schedule import pivot_schedule
 from season.projections import projections
 from season.league import League
+from season.goalies import possible, available
+
+# SET UP
+
+league, team = 84778, 12
+
+# SHEDULE - PULL ONCE
 
 schedule = pivot_schedule()
 
-league, team = 84778, 12
+# JOIN - EVERY LEAGUE
+
 proj = projections(league, team)
 df = pd.merge(proj, schedule, how="left", on="team")
 
-# Checkup
+# SKATERS
 
 league = League(df)
 league.team
 league.fill()
 league.check_all("")
 
-league.available.sort_values("off_day_score", ascending=False)
+# GOALIES - PULL ONCE
 
+shortlist = possible()
 
-##
+# GOALIES - TWEAK
+
+league = 84919
+
+yag = available(league)
+pd.merge(shortlist, yag, on="name", how="inner")
 
 
 
